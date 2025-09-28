a1022b19143df04322e735e1284a421aeb37cfe3
xr1s0nx-wave <artem@wave-gta5.com>
2025-09-28 06:06:12 +0300

    FEAT: init project and setup eslint config

---
+++ eslint.config.js
@@ -0,0 +1,114 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import importPlugin from 'eslint-plugin-import'
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y'
+import reactPlugin from 'eslint-plugin-react'
+
+export default [
+  { ignores: ['dist'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'import': importPlugin,
+      'jsx-a11y': jsxA11yPlugin,
+      'react': reactPlugin,
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        typescript: {},
+      },
+    },
+    rules: {
+      // React Hooks rules
+      ...reactHooks.configs.recommended.rules,
+      
+      // React Refresh rules
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      
+      // Airbnb-style rules (основные правила из конфигурации Airbnb)
+      'import/no-unresolved': 'off', // Отключаем для TypeScript
+      'import/extensions': 'off', // Отключаем для TypeScript
+      'import/prefer-default-export': 'off',
+      'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+      
+      // React rules
+      'react/react-in-jsx-scope': 'off', // React 17+ не требует импорта React
+      'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+      'react/function-component-definition': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react/prop-types': 'off', // TypeScript заменяет prop-types
+      'react/require-default-props': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/jsx-uses-vars': 'error',
+      
+      // JSX A11y rules
+      'jsx-a11y/alt-text': 'error',
+      'jsx-a11y/anchor-has-content': 'error',
+      'jsx-a11y/anchor-is-valid': 'error',
+      'jsx-a11y/aria-props': 'error',
+      'jsx-a11y/aria-proptypes': 'error',
+      'jsx-a11y/aria-unsupported-elements': 'error',
+      'jsx-a11y/click-events-have-key-events': 'warn',
+      'jsx-a11y/heading-has-content': 'error',
+      'jsx-a11y/html-has-lang': 'error',
+      'jsx-a11y/iframe-has-title': 'error',
+      'jsx-a11y/img-redundant-alt': 'error',
+      'jsx-a11y/no-access-key': 'error',
+      'jsx-a11y/no-redundant-roles': 'error',
+      'jsx-a11y/role-has-required-aria-props': 'error',
+      'jsx-a11y/role-supports-aria-props': 'error',
+      'jsx-a11y/scope': 'error',
+      'jsx-a11y/tabindex-no-positive': 'error',
+      
+      // Общие правила в стиле Airbnb
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      'no-unused-vars': 'off', // Используем TypeScript версию
+      '@typescript-eslint/no-unused-vars': 'error',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'object-shorthand': 'error',
+      'prefer-template': 'error',
+      'template-curly-spacing': 'error',
+      'arrow-spacing': 'error',
+      'comma-dangle': ['error', 'always-multiline'],
+      'comma-spacing': 'error',
+      'comma-style': 'error',
+      'computed-property-spacing': 'error',
+      'func-call-spacing': 'error',
+      'key-spacing': 'error',
+      'keyword-spacing': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'semi': ['error', 'always'],
+      'semi-spacing': 'error',
+      'space-before-blocks': 'error',
+      'space-before-function-paren': ['error', 'never'],
+      'space-in-parens': 'error',
+      'space-infix-ops': 'error',
+      'space-unary-ops': 'error',
+      'spaced-comment': 'error',
+    },
+  },
+]
